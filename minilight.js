669fa46fc763695bea6a6aeb9877b805f269fd1e
Mike Reinstein <reinstein.mike@gmail.com>
2017-02-20 22:13:15 -0800

    minor tweaks

--- minilight.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-// TODO: break up calculation into timeslices or use webworkers
-function minilight(image, iterations, camera, scene, random) {
-  for (var frameNum = 1; frameNum <= iterations; ++frameNum)
-    camera.getFrame(scene, image, random);
-  return image;
-}
